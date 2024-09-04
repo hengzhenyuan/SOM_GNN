cf1c0ec7ee16024169fa1f1a76afb90b7216b548
hengzhenyuan <101212914+hengzhenyuan@users.noreply.github.com>
2024-09-04 14:43:09 +0200

    Update model.py

--- model.py
+++ model.py
@@ -6,52 +6,6 @@ from torch_geometric.nn import RGCNConv, global_mean_pool
 
 
 
-
-
-# class RGCNmodel(torch.nn.Module):
-#     def __init__(self, in_channels, hidden_channels, out_channels, num_relations):
-#         super(RGCNmodel, self).__init__()
-#         self.conv1 = RGCNConv(in_channels, hidden_channels, num_relations)
-#         self.conv2 = RGCNConv(hidden_channels, out_channels, num_relations
-#                           )
-#
-#     def forward(self, data):
-#         x, edge_index, edge_type = data.x, data.edge_index,data.edge_attr.to(torch.int64).squeeze()
-#         batch= data.batch
-#         x = self.conv1(x, edge_index, edge_type)
-#         # x = F.relu(x)
-#         x = self.conv2(x, edge_index, edge_type)
-#
-#         x = global_mean_pool(x,batch)
-#
-#         return x
-
-
-# class RGCNmodel(torch.nn.Module):
-#     def __init__(self, in_channels, hidden_channels, out_channels, num_relations):
-#         super(RGCNmodel, self).__init__()
-#         self.conv1 = RGCNConv(in_channels, hidden_channels, num_relations)
-#         self.conv2 = RGCNConv(hidden_channels, out_channels, num_relations)
-#
-#     def forward(self, data):
-#         x, edge_index, edge_type = data.x, data.edge_index, data.edge_attr.to(torch.int64).squeeze()
-#         batch = data.batch
-#
-#         # First convolutional layer
-#         x = self.conv1(x, edge_index, edge_type)
-#         x = F.relu(x)
-#         x = F.dropout(x, p=0.5, training=self.training)
-#
-#         # Second convolutional layer
-#         node_features = self.conv2(x, edge_index, edge_type)
-#
-#         # Global mean pooling for graph-level representation
-#         graph_representation = global_mean_pool(node_features, batch)
-#
-#         return node_features, graph_representation
-#
-#
-
 class RGCNmodel(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, num_relations):
         super(RGCNmodel, self).__init__()
